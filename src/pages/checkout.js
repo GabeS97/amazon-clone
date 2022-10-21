3a19be80946840a48961d831984074f466c01e3b
Gabriel Sitorus <sitorusgg@gmail.com>
2022-10-21 21:12:10 +0700

    config modificaiton vercelk

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -10,7 +10,7 @@ function Checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const { data } = useSession();
-    console.log(data);
+    
     return (
         <div className='bg-gray-100 '>
             <Header />
