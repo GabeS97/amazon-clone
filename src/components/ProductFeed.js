25f33acc5d00c132267f33e14fe5d9ffcfeb2d35
Gabriel Sitorus <sitorusgg@gmail.com>
2022-10-21 11:23:30 +0700

    product feed built

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,8 +3,34 @@ import Product from './Product'
 
 function ProductFeed({ products }) {
     return (
-        <div>
-            {products?.map(({ id, title, price, description, category, image, rating }) => (
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+            {products?.slice(0, 4)?.map(({ id, title, price, description, category, image, rating }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image} rating={rating} />
+            ))}
+
+            <img className='md:col-span-full' src="http://links.papareact.com/dyz" alt="" />
+
+            <div className='md:col-span-2 '>
+            {products?.slice(4, 5)?.map(({ id, title, price, description, category, image, rating }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image} rating={rating} />
+            ))}
+            </div>
+
+            {products?.slice(5, products.length)?.map(({ id, title, price, description, category, image, rating }) => (
                 <Product
                     key={id}
                     id={id}
@@ -15,6 +41,7 @@ function ProductFeed({ products }) {
                     image={image} rating={rating} />
             ))}
         </div>
+
 
     )
 }
